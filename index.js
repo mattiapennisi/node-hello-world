7a8f7b0f6b7c3c8aa7cb3234857e027d34a7b4e1
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-11 13:15:31 +0100

    add watch shortcut to package.json

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /* 
-Impostiamo un nuovo script “watch” in package.json che possa essere lanciato con npm run watch e che aggiorni in tempo reale le modifiche ai nostri file. Lanciamolo e proviamo a cambiare il nostro codice in modo che stampi nel terminale “Hello Boolean”. Dovremmo vedere il terminale senza fermare e rilanciare il server.
 Bonus
 Proviamo a modificare la nostra app Node, in modo da passare dal terminale una frase diversa da “Hello Boolean”.
 Forse potrà servirvi qualcosa vista ieri! :occhiolino:
 Buon divertimento e confermate lettura come al solito
 */
 
-console.log('Hello World');
+console.log('Hello Boolean');
 
